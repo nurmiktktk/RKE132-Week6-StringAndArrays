059d93a6710cce798fa73a06154346f60201d7ef
Matias H. Nurmik <matiasnurmik@hotmail.com>
2024-12-18 21:33:36 +0200

    add week6 code

---
+++ Week6StringAndArrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
+
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.3.32825.248
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Week6StringAndArrays", "Week6StringAndArrays\Week6StringAndArrays.csproj", "{BFBDDB34-57D8-4619-8AD4-FD13ABD66D2E}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Arrays", "Arrays\Arrays.csproj", "{E7197D69-7A98-4561-B312-E3CCC5236745}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "NumberArray", "NumberArray\NumberArray.csproj", "{BFC77714-6923-4AE2-9995-4D6F3BE4A381}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "RandomElement", "RandomElement\RandomElement.csproj", "{38DE6957-D405-44F1-AAB1-3BB057306E8F}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{BFBDDB34-57D8-4619-8AD4-FD13ABD66D2E}.Debug | Any CPU.ActiveCfg = Debug | Any CPU
+		{BFBDDB34-57D8-4619-8AD4-FD13ABD66D2E}.Debug | Any CPU.Build.0 = Debug | Any CPU
+		{BFBDDB34-57D8-4619-8AD4-FD13ABD66D2E}.Release | Any CPU.ActiveCfg = Release | Any CPU
+		{BFBDDB34-57D8-4619-8AD4-FD13ABD66D2E}.Release | Any CPU.Build.0 = Release | Any CPU
+		{E7197D69-7A98-4561-B312-E3CCC5236745}.Debug | Any CPU.ActiveCfg = Debug | Any CPU
+		{E7197D69-7A98-4561-B312-E3CCC5236745}.Debug | Any CPU.Build.0 = Debug | Any CPU
+		{E7197D69-7A98-4561-B312-E3CCC5236745}.Release | Any CPU.ActiveCfg = Release | Any CPU
+		{E7197D69-7A98-4561-B312-E3CCC5236745}.Release | Any CPU.Build.0 = Release | Any CPU
+		{BFC77714-6923-4AE2-9995-4D6F3BE4A381}.Debug | Any CPU.ActiveCfg = Debug | Any CPU
+		{BFC77714-6923-4AE2-9995-4D6F3BE4A381}.Debug | Any CPU.Build.0 = Debug | Any CPU
+		{BFC77714-6923-4AE2-9995-4D6F3BE4A381}.Release | Any CPU.ActiveCfg = Release | Any CPU
+		{BFC77714-6923-4AE2-9995-4D6F3BE4A381}.Release | Any CPU.Build.0 = Release | Any CPU
+		{38DE6957-D405-44F1-AAB1-3BB057306E8F}.Debug | Any CPU.ActiveCfg = Debug | Any CPU
+		{38DE6957-D405-44F1-AAB1-3BB057306E8F}.Debug | Any CPU.Build.0 = Debug | Any CPU
+		{38DE6957-D405-44F1-AAB1-3BB057306E8F}.Release | Any CPU.ActiveCfg = Release | Any CPU
+		{38DE6957-D405-44F1-AAB1-3BB057306E8F}.Release | Any CPU.Build.0 = Release | Any CPU
+    EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {00E6B097-4AEC-4041-AF83-480CA06057F9}
+	EndGlobalSection
+EndGlobal
